fbc5049d7f96b54dbad8bef7e5d241c278fb313a
Andrea Frasca <137279635+Gold18K@users.noreply.github.com>
2023-12-13 11:18:36 +0100

    Update Scripts.js

--- Scripts/Scripts.js
+++ Scripts/Scripts.js
@@ -1,150 +1,15 @@
 
-// Classes
-class Web_Workers_Queue {
-
-    // Constructor
-    constructor(_worker_path, _number_of_workers, _on_message) {
-        this.#number_of_workers = _number_of_workers;
-        this.#jobs            = [];
-        this.#workers         = [];
-
-        for (let i = 0; i != _number_of_workers; ++i)
-            this.#workers.push(new Worker(_worker_path));
-
-        for (let i = 0; i != _number_of_workers; ++i) {
-            this.#workers[i].onmessage = (_event) => {
-                _on_message(_event);
-
-                Web_Workers_Queue.#free_worker(this, _event.data.id);
-                Web_Workers_Queue.#check_available_job(this);
-            };
-        }
-        
-        this.#current_jobs          = Array.from({ length: _number_of_workers }, () => null);
-        this.#avaliable_workers_IDs = [];
-
-        for (let i = 0; i != _number_of_workers; ++i)
-            this.#avaliable_workers_IDs.push(i);
-
-        this.#on_message  = _on_message;
-        this.#worker_path = _worker_path;
-    }
-
-    // Functions
-    send_job(_job) { 
-        const worker_ID = this.#avaliable_workers_IDs.pop();
-
-        if (worker_ID === undefined) {
-            this.#jobs.push(_job);
-            return;
-        }
-
-        this.#workers[worker_ID].postMessage({
-            id:   worker_ID,
-            work: _job
-        });
-
-        this.#current_jobs[worker_ID] = _job;
-    }
-    terminate_workers() {
-
-        for (let i = 0; i != this.#number_of_workers; ++i)
-            this.#workers[i].terminate();
-
-        this.#jobs    = [];
-        this.#workers = [];
-
-        for (let i = 0; i != this.#number_of_workers; ++i)
-            this.#workers.push(new Worker(this.#worker_path));
-
-        for (let i = 0; i != this.#number_of_workers; ++i) {
-            this.#workers[i].onmessage = (_event) => {
-                this.#on_message(_event);
-
-                Web_Workers_Queue.#free_worker(this, _event.data.id);
-                Web_Workers_Queue.#check_available_job(this);
-            };
-        }
-        
-        this.#current_jobs          = Array.from({ length: this.#number_of_workers }, () => null);
-        this.#avaliable_workers_IDs = [];
-
-        for (let i = 0; i != this.#number_of_workers; ++i)
-            this.#avaliable_workers_IDs.push(i);
-
-    }
-    change_number_of_workers(_new_number_of_workers) {
-
-        if (this.#number_of_workers === _new_number_of_workers)
-            return;
-
-        if (this.#number_of_workers < _new_number_of_workers) {
-
-            for (let i = this.#number_of_workers; i != _new_number_of_workers; ++i) {
-                this.#avaliable_workers_IDs.push(i);
-                this.#workers.push(new Worker('Scripts/Section_Generator.js'));
-                
-                this.#workers[i].onmessage = (_event) => {
-                    this.#on_message(_event);
-    
-                    Web_Workers_Queue.#free_worker(this, _event.data.id);
-                    Web_Workers_Queue.#check_available_job(this);
-                };
-
-                Web_Workers_Queue.#check_available_job(this);
-            }
-
-        }
-
-        if (this.#number_of_workers > _new_number_of_workers) {
-
-            for (let i = this.#number_of_workers - 1; i != _new_number_of_workers - 1; --i) {
-                this.#workers[i].terminate();
-
-                this.#workers.pop();
-
-                this.#jobs.unshift(this.#current_jobs[i]);
-
-                this.#current_jobs.pop();
-            }
-
-        }
-
-        this.#number_of_workers = _new_number_of_workers;
-    }
-    jobs_left() {
-        return this.#jobs.length + this.#number_of_workers - this.#avaliable_workers_IDs.length;
-    }
-    static #free_worker(_instance, _id) {
-        _instance.#avaliable_workers_IDs.push(_id);
-
-        _instance.#current_jobs[_id] = null;
-    }
-    static #check_available_job(_instance) {
-
-        if (_instance.#jobs.length === 0)
-            return;
-
-        _instance.send_job(_instance.#jobs.pop()); // This should be shift(), not pop()!
-    }
-
-    // Fields
-    #number_of_workers;
-    #jobs;
-    #workers;
-    #current_jobs;
-    #avaliable_workers_IDs;
-    #worker_path;
-    #on_message;
-
-}
+// Import
+import {
+    Web_Workers_Queue
+} from "./Web_Workers_Queue.mjs"
 
 // Variables
 let MILLER_RABIN_REPETITIONS = 6;
 let WORKERS_WORK_SIZE        = 150;
 let NUMBER_OF_CORES_IN_USE   = Math.max(1, navigator.hardwareConcurrency - 1);
-let CANVAS_SIDE_LENGTH_X     = Math.floor(window.innerWidth / 1);
-let CANVAS_SIDE_LENGTH_Y     = Math.floor(window.innerHeight / 1);
+let CANVAS_SIDE_LENGTH_X     = Math.floor(window.innerWidth);
+let CANVAS_SIDE_LENGTH_Y     = Math.floor(window.innerHeight);
 let MOUSE_CURRENT_X          = 0;
 let MOUSE_CURRENT_Y          = 0;
 let MOUSE_DOWN_START_X       = 0;
@@ -156,7 +21,7 @@ let MOVING_CANVAS_IMAGE_DATA = null;
 // Constants
 const COMPOSITE_COLOR  = "rgb(25, 25, 25)"
 const ERROR_CONTOUR    = "2px solid rgb(255, 70, 70)"
-const WEB_WORKER_QUEUE = new Web_Workers_Queue('Scripts/Section_Generator.js', NUMBER_OF_CORES_IN_USE, function(_event) {
+const WEB_WORKER_QUEUE = new Web_Workers_Queue('Scripts/Section_Generator.mjs', NUMBER_OF_CORES_IN_USE, function(_event) {
     const ctx = document.getElementById('Ulam_Canvas').getContext('2d', { willReadFrequently: true });
 
     let imageData = ctx.createImageData(_event.data.grid[0].length, _event.data.grid.length);
@@ -182,7 +47,7 @@ const WEB_WORKER_QUEUE = new Web_Workers_Queue('Scripts/Section_Generator.js', N
     }
 
     if (MOVING_CANVAS_IMAGE_DATA !== null) {
-
+        
         for (let y = 0; y < _event.data.grid.length; y++) {
 
             for (let x = 0; x < _event.data.grid[0].length; x++) {
